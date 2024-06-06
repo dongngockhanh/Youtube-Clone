a2c693620b7cd6edfae7b87595acfece86465f40
dongngockhanh <dnkhanh.12a3@gmail.com>
2024-06-07 06:04:46 +0700

    create back-end

---
+++ Back-End/Youtobe-Clone/src/main/java/com/example/Youtobe_Clone/YoutobeCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.Youtobe_Clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutobeCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutobeCloneApplication.class, args);
+	}
+
+}
---
+++ Back-End/Youtobe-Clone/src/test/java/com/example/Youtobe_Clone/YoutobeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Youtobe_Clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutobeCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
